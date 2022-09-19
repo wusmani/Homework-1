18327c804deed3010eb9facb0f72a262e20d8e4c
wusmani <112104701+wusmani@users.noreply.github.com>
2022-09-19 13:57:10 -0500

    Add files via upload

---
+++ 1.20 ZyLabs.py
@@ -0,0 +1,13 @@
+
+user_num = int(input("Enter integer:\n"))
+print("You entered:", user_num)
+print(user_num, "squared is", user_num*user_num)
+print("And", user_num, "cubed is", user_num*user_num*user_num,"!!")
+another_num = int(input("Enter another integer:\n"))
+print(user_num, '+',another_num,'is', user_num+another_num)
+print(user_num,"*", another_num, "is", user_num*another_num)
+
+
+
+
+
